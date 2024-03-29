dda13c479df4c51871795fafc80d092ecf5c3748
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-03-29 13:37:48 +0300

    Added animations

--- Assets/_Assets/Scripts/PlayerController.cs
+++ Assets/_Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float rotateSpeed;
 
+    private bool isWalking;
 
     private void Update()
     {
@@ -39,6 +40,14 @@ public class PlayerController : MonoBehaviour
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        isWalking = moveDir != Vector3.zero;
+
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }

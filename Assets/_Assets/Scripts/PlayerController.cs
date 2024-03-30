5d0ec3938ddbb3b8ddc8529394b231d5958152bf
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-03-30 12:27:29 +0300

    Update PlayerController.cs

--- Assets/_Assets/Scripts/PlayerController.cs
+++ Assets/_Assets/Scripts/PlayerController.cs
@@ -34,8 +34,6 @@ public class PlayerController : MonoBehaviour
             Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
 
-
-
             if (canMove)
             {
                 //Can only on the X
@@ -48,20 +46,25 @@ public class PlayerController : MonoBehaviour
 
                 //Attempt only Z movement
                 Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
-                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
 
                 if (canMove)
                 {
                     //Can move only on the Z
                     moveDir = moveDirZ;
                 }
-
+                 
                 else
                 {
                     //Cannot move any direction
                 }
             }
         }
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
+
         isWalking = moveDir != Vector3.zero;
 
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);

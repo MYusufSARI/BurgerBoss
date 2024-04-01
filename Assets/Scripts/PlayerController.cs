afba5e0d277287984115e9d8dca37756f2720d24
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-04-01 07:22:44 +0300

    Events

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,6 +18,7 @@ public class PlayerController : MonoBehaviour
 
     private bool isWalking;
     private Vector3 lastInteractDir;
+    private ClearCounter selectedCounter;
 
     private void Start()
     {
@@ -26,33 +27,9 @@ public class PlayerController : MonoBehaviour
 
     private void GameInput_OnInteractAction(object sender, System.EventArgs e)
     {
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-
-        if (moveDir != Vector3.zero)
+        if (selectedCounter is { } )
         {
-            lastInteractDir = moveDir;
-        }
-
-        float interactDistance = 1f;
-        RaycastHit raycastHit;
-        if (Physics.Raycast(transform.position, lastInteractDir, out raycastHit, interactDistance, countersLayerMask))
-        {
-            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
-            {
-                //Has Clear Counter
-                clearCounter.Interact();
-            }
-
-            /*
-            Another way of using Getcomponent but we usually prefer
-            TryGetComponent because of the null check  
-            ClearCounter clearCounter = raycastHit.transform.GetComponent<ClearCounter>();
-            if (clearCounter !=null)
-            {
-                //Has Clear Counter
-            }
-            */
+            selectedCounter.Interact();
         }
     }
 
@@ -84,6 +61,14 @@ public class PlayerController : MonoBehaviour
             if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
             {
                 //Has Clear Counter
+                if (clearCounter !=selectedCounter)
+                {
+                    selectedCounter = clearCounter;
+                }
+            }
+            else
+            {
+                selectedCounter = null;
             }
 
             /*
@@ -96,6 +81,12 @@ public class PlayerController : MonoBehaviour
             }
             */
         }
+        else
+        {
+            selectedCounter = null;
+        }
+
+        Debug.Log(selectedCounter);
     }
 
 

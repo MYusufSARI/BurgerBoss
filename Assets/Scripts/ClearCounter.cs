f3c0932377df5ff4f7877e40255deb364cde3f4b
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-04-01 14:05:34 +0300

    .

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -27,9 +27,7 @@ public class ClearCounter : MonoBehaviour
             if (kitchenObject != null)
             {
                 kitchenObject.SetClearCounter(secondClearCounter);
-                Debug.Log(kitchenObject.GetClearCounter());
             }
-
         }
     }
 
@@ -39,9 +37,7 @@ public class ClearCounter : MonoBehaviour
         if (kitchenObject == null)
         {
             Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-            kitchenObjectTransform.localPosition = Vector3.zero;
-            kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
-            kitchenObject.SetClearCounter(this);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
         }
 
         else
@@ -54,5 +50,25 @@ public class ClearCounter : MonoBehaviour
     public Transform GetKitchenObjectFollowTransform()
     {
         return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
     }
 }

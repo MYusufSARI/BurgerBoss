94bdfb55149c3a60bfcaf3a1da503667f7b5abf2
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-04-03 19:31:36 +0300

    .

--- Assets/Scripts/UI/ProgressBarUI.cs
+++ Assets/Scripts/UI/ProgressBarUI.cs
@@ -1,8 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ProgressBarUI : MonoBehaviour
 {
-    
+    [SerializeField]
+    private Image barImage;
+
+    [SerializeField]
+    private CuttingCounter cuttingCounter;
 }

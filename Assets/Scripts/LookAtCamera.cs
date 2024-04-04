be24e66d451a929dc0e7a7776d0857564d017f69
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-04-04 19:34:19 +0300

    Update LookAtCamera.cs

--- Assets/Scripts/LookAtCamera.cs
+++ Assets/Scripts/LookAtCamera.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class LookAtCamera : MonoBehaviour
 {
+    public enum Mode
+    {
+        LookAt,
+        LookAtInverted,
+    }
+
+    [SerializeField]
+    private Mode mode;
+
     private void LateUpdate()
     {
         transform.LookAt(Camera.main.transform);

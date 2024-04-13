39936949518c0c3ac5c79309d4f1fad3b1fbb80b
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-04-13 13:01:57 +0300

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -83,8 +83,6 @@ public class Player : MonoBehaviour, IKitchenObjectParent
         if (moveDir != Vector3.zero)
         {
             lastInteractDir = moveDir;
-            lastInteractDir = moveDir;
-
         }
 
         float interactDistance = 2f;

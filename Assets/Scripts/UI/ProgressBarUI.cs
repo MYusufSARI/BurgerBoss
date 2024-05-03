183b142c70a75f03648a6f9897387ee039b08751
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-05-03 09:28:16 +0300

    .

--- Assets/Scripts/UI/ProgressBarUI.cs
+++ Assets/Scripts/UI/ProgressBarUI.cs
@@ -9,18 +9,27 @@ public class ProgressBarUI : MonoBehaviour
     private Image barImage;
 
     [SerializeField]
-    private CuttingCounter cuttingCounter;
+    private GameObject hasProgressGameObject;
+
+    private IHasProgress hasProgress;
 
     private void Start()
     {
-        cuttingCounter.OnProgressChanged += CuttingCounter_OnProgressChanged;
+        hasProgress = hasProgressGameObject.GetComponent<IHasProgress>();
+        if (hasProgress == null)
+        {
+            Debug.LogError("GameObject" + hasProgressGameObject +
+                           "does not have a component that implements IHasProgress!");
+        }
+
+        hasProgress.OnProgressChanged += HasProgress_OnProgressChanged;
 
         barImage.fillAmount = 0f;
 
         Hide();
     }
 
-    private void CuttingCounter_OnProgressChanged(object sender, CuttingCounter.OnProgressChangedEventArgs e)
+    private void HasProgress_OnProgressChanged(object sender, IHasProgress.OnProgressChangedEventArgs e)
     {
         barImage.fillAmount = e.progressNormalized;
 

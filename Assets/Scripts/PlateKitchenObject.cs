9b247d254be7ea57eaf8728f101e6c3a8bb2dfc8
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-05-10 04:05:23 +0300

    Update PlateKitchenObject.cs

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlateKitchenObject : MonoBehaviour
+public class PlateKitchenObject : KitchenObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }

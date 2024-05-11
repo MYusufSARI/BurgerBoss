6e231a4e91b8eaf40b8a0181681cff725142d70e
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-05-11 06:06:54 +0300

    UI detailed

--- Assets/Scripts/UI/PlateIconsUI.cs
+++ Assets/Scripts/UI/PlateIconsUI.cs
@@ -6,4 +6,25 @@ public class PlateIconsUI : MonoBehaviour
 {
     [SerializeField]
     private PlateKitchenObject plateKitchenObject;
+
+    [SerializeField]
+    private Transform iconTemplate;
+
+    private void Start()
+    {
+        plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
+    }
+
+    private void PlateKitchenObject_OnIngredientAdded(object sender, PlateKitchenObject.OnIngredientAddedEventArgs e)
+    {
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        foreach (KitchenObjectSO kitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList())
+        {
+
+        }
+    }
 }

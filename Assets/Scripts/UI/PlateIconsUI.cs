3b0f875fab51e6816b549246762b2ad506367a6b
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-05-11 06:21:29 +0300

    Added Icons

--- Assets/Scripts/UI/PlateIconsUI.cs
+++ Assets/Scripts/UI/PlateIconsUI.cs
@@ -10,6 +10,11 @@ public class PlateIconsUI : MonoBehaviour
     [SerializeField]
     private Transform iconTemplate;
 
+    private void Awake()
+    {
+        iconTemplate.gameObject.SetActive(false);
+    }
+
     private void Start()
     {
         plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
@@ -22,9 +27,17 @@ public class PlateIconsUI : MonoBehaviour
 
     private void UpdateVisual()
     {
+        foreach (Transform child in transform)
+        {
+            if (child == iconTemplate) continue;
+            Destroy(child.gameObject);
+        }
+
         foreach (KitchenObjectSO kitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList())
         {
             Transform iconTransform =  Instantiate(iconTemplate, transform);
+            iconTransform.gameObject.SetActive(true);
+            iconTransform.GetComponent<PlateIconSingleUI>().SetKitchenObjectSO(kitchenObjectSO);
         }
     }
 }

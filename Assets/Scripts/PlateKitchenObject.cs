8c1825477fcc51dd11948d0b47299fa978054b06
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-05-11 04:49:19 +0300

    Update PlateKitchenObject.cs

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,9 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+    public event EventHandler<OnIngredientAddedEventArgs> OnInIngredientAdded;
+
+    public class OnIngredientAddedEventArgs : EventArgs
+    {
+        public KitchenObjectSO kitchenObjectSO;
+    }
+
     [SerializeField]
     private List<KitchenObjectSO> validkitchenObjectSOList;
 

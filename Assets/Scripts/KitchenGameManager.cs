e63048cafa59bc0513a9f666ff184d24979be223
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2024-05-14 23:32:23 +0300

    Added GameOverUI

--- Assets/Scripts/KitchenGameManager.cs
+++ Assets/Scripts/KitchenGameManager.cs
@@ -20,7 +20,9 @@ public class KitchenGameManager : MonoBehaviour
     private State state;
     private float waitingToStartTimer = 1f;
     private float countDownToStartTimer = 3f;
-    private float gamePlayingTimer = 10f;
+    private float gamePlayingTimer;
+    private float gamePlayingTimerMax = 10f;
+
 
     private void Awake()
     {
@@ -46,6 +48,7 @@ public class KitchenGameManager : MonoBehaviour
                 if (countDownToStartTimer < 0f)
                 {
                     state = State.GamePlaying;
+                    gamePlayingTimer = gamePlayingTimerMax;
                     OnStateChanged?.Invoke(this, EventArgs.Empty);
 
                 }
@@ -88,4 +91,8 @@ public class KitchenGameManager : MonoBehaviour
         return state == State.GameOver;
     }
 
+    public float GetGamePlayingTimerNormalized()
+    {
+        return 1 - (gamePlayingTimer / gamePlayingTimerMax);
+    }
 }
